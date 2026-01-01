a38a76f59d0462bc10128b1924040a0662d8b85c
tabsheermk <creativemk12@gmail.com>
2026-01-01 12:01:06 +0530

    db connected

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -3,9 +3,9 @@ import Config
 # Configure your database
 config :backend, Backend.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Root12#",
   hostname: "localhost",
-  database: "backend_dev",
+  database: "inventory_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
