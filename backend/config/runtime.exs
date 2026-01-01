18a94a3c170762d760091cf7e013195092069abd
Mohd Khaja Tabsheeruddin <75476341+tabsheermk@users.noreply.github.com>
2026-01-01 16:50:52 +0000

    syntax error fixed

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -36,7 +36,7 @@ if config_env() == :prod do
   config :backend, Backend.Repo,
     ssl: true,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
     # socket_options: maybe_ipv6
