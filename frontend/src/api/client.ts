f4751e818eb42aff40ae82a42ed78b1bad5d7d53
Mohd Khaja Tabsheeruddin <75476341+tabsheermk@users.noreply.github.com>
2026-01-01 16:12:19 +0000

    docker file

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000/api";
+const API_URL = "/api";
 
 export async function api<T>(path: string, options?: RequestInit): Promise<T> {
   const res = await fetch(`${API_URL}${path}`, {
